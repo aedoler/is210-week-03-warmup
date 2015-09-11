d3db17c57088476730858d325c15d0263dddaec2
Alex Doler <aedoler@gmail.com>
2015-09-11 03:56:46 +0000

    fractions

---
+++ task_12.py
@@ -0,0 +1,11 @@
+#!user/bin/env python
+# -*- coding: utf-8 -*-
+"""docstring"""
+
+import decimal
+import fractions
+
+INTVAL = 1
+FLOATVAL = 0.1
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = fractions.Fraction(1, 10)
