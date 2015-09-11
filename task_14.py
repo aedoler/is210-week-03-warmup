fc919fecd5af80d27d3352b80fce5ed214ca4d41
Alex Doler <aedoler@gmail.com>
2015-09-11 04:43:45 +0000

    True False

---
+++ task_14.py
@@ -0,0 +1,11 @@
+#!user/bin/env python
+# -*- coding: utf-8 -*-
+"""docstring"""
+
+IS_TRUE = True
+IS_FALSE = False
+IS_NONE = None
+
+print IS_TRUE == 1, IS_FALSE and 0
+
+INTEGER_EQUIV = 'True False'
